4ca636530215ea2bad06ea552b0c28a3280212d4
harsh <harshranjan1995@gmail.com>
2020-10-26 14:36:07 +0530

    second  commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    print(request.args.get("name"))
+    return render_template('index.html', act_name=request.args.get("name"))
 
 
 if __name__ == "__main__":
